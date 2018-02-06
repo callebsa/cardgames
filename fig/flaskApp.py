315bfa8e19279312d97c4c69265c53aa1e2b605c
callebsa <callebsa@github.com>
2018-02-06 19:36:25 +0100

    Added image suport

--- fig/flaskApp.py
+++ fig/flaskApp.py
@@ -19,4 +19,4 @@ def get_img(filename):
     return "File :" + filename + "cannot be find";
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=80, debug=True)
+	app.run(host='0.0.0.0', port=5000, debug=True)
