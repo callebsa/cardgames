e78e44d901dd7e026a12e9d70885468e1cad57ce
callebsa <callebsa@github.com>
2018-02-06 19:35:08 +0100

    Added image suport

--- fig/flaskApp.py
+++ fig/flaskApp.py
@@ -7,19 +7,16 @@ def hello_world():
 
 @app.route('/vandtia/img/<path:filename>')
 def get_img(filename):
-	    try:
-      		w = int(request.args['w'])
-        	h = int(request.args['h'])
-	    except (KeyError, ValueError):
-    	    return send_from_directory('.', filename)
- 		try:
-        	im = Image.open(filename)
-        	#im.thumbnail((w, h), Image.ANTIALIAS)
-       		io = StringIO.StringIO()
-        	im.save(io, format='JPEG')
-     	   return Response(io.getvalue(), mimetype='image/jpeg')
-    except IOError:
-        return "File :" + filename + "cannot be find";
+  w= int(request.args['w'])
+  h= int(request.args['h'])
+  try:
+    im = Image.open(filename)
+    #im.thumbnail((w, h), Image.ANTIALIAS)
+    io = StringIO.StringIO()
+    im.save(io, format='JPEG')
+    return Response(io.getvalue(), mimetype='image/jpeg')
+  except IOError:
+    return "File :" + filename + "cannot be find";
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80, debug=True)
